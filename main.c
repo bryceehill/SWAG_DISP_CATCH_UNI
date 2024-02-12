9f6def9cc0c005afcf3bdca4d134d71134d1830e
dustinmarquardt <146291322+dustinmarquardt@users.noreply.github.com>
2024-02-12 14:53:34 -0700

    Add files via upload

--- main.c
+++ main.c
@@ -1,8 +1,7 @@
 #include <msp430.h> 
 #include "serial_handler.h"
-#include "uart.h"
 #include "main.h"
-#include <stdint.h>
+//#include <stdint.h>
 
 //#include "uart.h"
 
@@ -37,301 +36,119 @@
  */
 
 char gridArray[5] = {0x7F, 0x04, 0x18, 0x04, 0x7F};
-char currChar = ' ';
-int isrCount = 0,scrollCount = 0;
+int isrCount = 0, scrollCount = 0;
+#define LOCKOUT_DURATION 5000 // Lockout duration in milliseconds
+int lockoutTimer = 0;
+int ModeSelect = 2;
+int TempMode = 2;
+#define ModeSelect_Duration 20000 // Timer for changing mode (press SW2)
+int ModeTimer = 3;
 
-int main(void){
-    WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+
+int main(void) {
+    WDTCTL = WDTPW | WDTHOLD; // stop watchdog timer
     led_init();
-    //    switch_init();
+    switch_init();
     timer_ISR_init();
-    uart_init(4); // 9600 baud rate = 4 input
-    uart_sw_init(); // Software uart init
-    BCSCTL1 = CALBC1_16MHZ;      // Set range
-    DCOCTL = CALDCO_16MHZ;      // SMCLK = DCO = 1MHz
-    uint8_t c;
-    int replyVal=0;
-    while(1){
-        if(eos_flag){
-            replyVal = serial_handler (rx_data_str , 0, rx_flag, tx_data_str);
-            if (replyVal)
-                uart_write_string(0,replyVal);
+    uart_init(4);
+    //uint8_t c;
+    int a_val, k_val, scrollPos=0, scrollNum, scrollMax, cat_val;
+    char disp_string[] = "   Welcome to the TechMatrix! Go to mtech.edu/electrical-engineering/ today!  |";//Green
+    char two_char[11] = {0,0,0,0,0,0,0,0,0,0,0};
+    char scrollLast=0;
+    a_val = 0;
+    while (scrollNum != '|')
+        scrollNum = disp_string[a_val++];
+    scrollNum=a_val;
+    scrollMax = scrollNum*6;
+
+    while (1) {
+        if (eos_flag) {
+
+            ModeSelect = 1;
+            //Reset lockout timer
+            lockoutTimer = 0;
+            // Controlled mode (catch)
+            __delay_cycles(100000);
             rx_flag = 0;
             eos_flag = 0;
+            char temp = gridArray[0];
+            temp |= BIT7;
+            gridArray[0] = gridArray[1];
+            gridArray[1] = gridArray[2];
+            gridArray[2] = gridArray[3];
+            gridArray[3] = gridArray[4];
+            cat_val = rx_data_str[5] & 0x0F;
+            cat_val <<= 4;
+            cat_val |= (rx_data_str[6] & 0x0F);
+            gridArray[4] = cat_val;
 
+            tx_data_str[0] = '%';
+            tx_data_str[1] = '*';
+            tx_data_str[2] = 'B';
+            tx_data_str[3] = '0';
+            tx_data_str[4] = '1';
+            tx_data_str[5] = 0x30 + (temp >> 4);
+            tx_data_str[6] = 0x30 + (temp & 0x0F);
+            uart_write_string(0, 7);
         }
-        if (eos_flag_sw){
-            replyVal = serial_handler (rx_data_str_sw ,1, rx_flag_sw, tx_data_str_sw+1);  // THe +1 in this line and the line below is due to a SW uart Glitch IDK
-            if (replyVal)
-                uart_write_string_sw(0,replyVal+1);
-
-            rx_flag_sw = 0;
-            eos_flag_sw = 0;
-
-
+        if (lockoutTimer > LOCKOUT_DURATION) {
+            lockoutTimer = LOCKOUT_DURATION+1;
+            ModeSelect = TempMode;
         }
+        if (ModeSelect == 2){
+            //            // Stand alone text scroll mode (send)
+            if (scrollCount >= 2000) {
+                scrollCount = 0;
+                a_val = scrollPos / 6;
+                charMap(disp_string[a_val], two_char);
+                a_val = scrollPos/6;
+                charMap(disp_string[a_val + 1], two_char + 6);
+                a_val = scrollPos%6;
+                tx_data_str[0] = '%';
+                tx_data_str[1] = '*';
+                tx_data_str[2] = 'B';
+                tx_data_str[3] = '0';
+                tx_data_str[4] = '1';
+                tx_data_str[5] = 0x30 + (gridArray[0] >> 4);
+                tx_data_str[6] = 0x30 + (gridArray[0] & 0x0F);
+                uart_write_string(0, 7);
 
-
-    }
-}
-
-void shiftIn(char * arrIn, char * arrOut,int shiftNum, int inDir){
-    // This takes the bytes from arrIn and puts them into arrout.  Direction is dependent upon inDir that can swap direction
-    int k;
-    if (inDir){ // direct copy values from arrIn to Arr out swapping order
-        for (k=0;k<shiftNum;k++){
-            arrOut[k] = arrIn[shiftNum-k];
-        }
-    }
-    else{     // direct copy of values
-        for (k=0;k<shiftNum;k++){
-            arrOut[k] = arrIn[k];
-        }
-    }
-
-}
-
-void shiftArr(char * arrIn,int byteNum,int shiftNum,int shiftDir){
-    int k,n;
-    if (shiftDir){      // Shift to the right
-        for (n=0;n<byteNum;n++){
-            for (k=0;k<shiftNum-1;k++){
-
-                arrIn[shiftNum-k]=arrIn[shiftNum-k-1];
+                for (k_val = 0; k_val < 5; k_val++) {
+                    gridArray[k_val] = two_char[k_val + a_val];
+                }
+                scrollPos++;
+                if (scrollPos > scrollMax)
+                    scrollPos = 0;
             }
         }
-    }
-    else{  //shift to the left
-        for (n=0;n<byteNum;n++){
-            for (k=0;k<shiftNum-1;k++){
-                arrIn[k]=arrIn[k+1];
-            }
+        if (ModeSelect == 3){
+            char gridArray[5] = {0x7F, 0x04, 0x18, 0x04, 0x04};
         }
 
     }
-
-}
-
-int serial_handler ( char * stringIn, int uartdir,int strLen,char * stringOut){
-    // Parse for the initial string and the end string
-    volatile int strBeg, strEnd,i,k,byteNum,byteRet,shiftNum,dispShift;
-    volatile char tempChar;
-    char cmpStr[2],byteOut[5],byteChar[5];
-    cmpStr[0]='%';
-    cmpStr[1]='*';
-    strBeg = strComp(stringIn,cmpStr,strLen+1,2);
-    if (strBeg == 0xFFFF)
-        return;
-    cmpStr[0]=10;
-    strEnd = strComp(stringIn,cmpStr,strLen+2,1);
-    if (strEnd == 0xFFFF)
-        return;
-    if (stringIn[strBeg+2] == 'b'){     // Bytes of data to display
-        shiftIn(stringIn, stringOut, 4, 0); // copy first four bytes no questions asked
-        byteNum = stringIn[strBeg+3];
-        if (uartdir){   // Up-stream UART
-            shiftIn(gridArray, stringIn+byteNum+4, 5, 0);   // copy the existing grid Array
-            shiftIn(stringIn+4,gridArray,5,0);      // Move last 5 bytes into grid Array
-            shiftIn(stringIn+9+strBeg, stringOut+4, byteNum, 0); // copy all bytes but shifted by 5
-
-        }
-        else{
-            shiftIn(stringIn+4+strBeg, stringOut+9, byteNum, 0); // copy all bytes but shifted by 5
-            shiftIn(gridArray, stringOut+4, 5, 0);   // copy the existing grid Array
-            shiftIn(stringOut+4+byteNum,gridArray,5,0);      // Move last 5 bytes into grid Array
-        }
-        //        if (byteNum>5){  // Pass on other data
-        //            stringOut[0] = stringIn[0+strBeg];
-        //            stringOut[1] = stringIn[1+strBeg];
-        //            stringOut[2] = stringIn[2+strBeg];
-        //            stringOut[3] = stringIn[3+strBeg];
-        //            for (k=0;k<byteNum-5;k++)  // move up the data to send
-        //                stringOut[k+4]=stringIn[9+strBeg];
-        //
-        //            shiftIn(gridArray,stringOut+k+byteNum,5,uartdir);  // append data from existing display
-        //
-        //            shiftIn(stringIn+4+strBeg,gridArray,5,uartdir);  //  new Data to display
-        //            return byteNum+4;
-        //        }
-        //        else{
-        //            shiftArr(gridArray,byteNum,5,uartdir);
-        //            shiftIn(stringIn+4+strBeg+5-byteNum,gridArray,byteNum,uartdir);  // New Data to display
-        //        }
-        return byteNum+4;
-
-
-
-    }
-    else if (stringIn[strBeg+2] == 'B'){     // Bytes of data to display
-        // Save this one for later
-        //        byteNum = twoCharConv (stringIn+strBeg+3);
-        //        convNibble(stringIn+strBeg+5,byteOut,5);
-        //        if (byteNum>5){     // Pass on the other info.
-        //            stringOut[0] = stringIn[0+strBeg];
-        //            stringOut[1] = stringIn[1+strBeg];
-        //            stringOut[2] = stringIn[2+strBeg];
-        //            num2Char2Digit(byteNum-5,stringIn+3);
-        //            for (k = 0;k<byteNum-5;k++){
-        //                stringOut[k+5] = stringIn[15+strBeg];
-        //                stringOut[k+6] = stringIn[16+strBeg];
-        //            }
-        //        }
-        //        if (byteNum > 5)
-        //            byteNum =5;
-        //        for (k = 0;k < byteNum; k++){ // step the existing grid to the side
-        //            if (uartdir){
-        //                gridArray[5-k] = gridArray[4-k];
-        //            }
-        //            else{
-        //                gridArray[k] = gridArray[k+1];
-        //            }
-        //        }
-        //        for (k = 0;k < byteNum; k++){   // move in the new data
-        //            if (uartdir){
-        //                gridArray[byteNum-k] = byteOut[k];
-        //            }
-        //            else{
-        //                gridArray[k+byteNum] = byteOut[k];
-        //            }
-        //        }
-        //        if (byteNum>5)  // Can't be bytenum if it's 5
-        //            return byteNum*2+3;
-        //
-
-    }
-    else if (stringIn[strBeg+2] == 'C'){    // Characters of data to display
-        shiftIn(stringIn, stringOut, 5, 0); // copy first Five bytes no questions asked
-        byteNum = twoCharConv (stringIn+strBeg+3);
-        if (uartdir){
-
-            shiftIn(&currChar,stringIn+byteNum+5,1,0);
-            currChar = stringIn[strBeg+5];
-            charMap(currChar, gridArray);
-            shiftIn(stringIn+6+strBeg,stringOut+5,byteNum,0);  // updating data 2 send to next downstream or upstream
-
-        }
-        else{
-            shiftIn(stringIn+5+strBeg,stringOut+6,byteNum-1,0);  // updating data 2 send to next downstream or upstream
-            stringOut[5] = currChar;
-            currChar = stringIn[4+byteNum+strBeg];
-            charMap(currChar, gridArray);
-
-        }
-        //        //        tempChar = stringIn[strBeg+7];
-        //
-        //        //        shiftNum = stringIn[strBeg+5]-0x30;  // get the number of times to shift the data character
-        //        //        dispShift = stringIn[strBeg+6]-0x30;  // get the number of times to shift the existing display
-        //        //        shiftArr(gridArray,5,5,uartdir);  // number of characters to shift the existing display
-        //        //        shiftIn(byteChar,gridArray+5-shiftNum,shiftNum,uartdir);  // New Data to display shifting in the desired number of bytes
-        //        stringOut[0] = stringIn[0+strBeg];
-        //        stringOut[1] = stringIn[1+strBeg];
-        //        stringOut[2] = stringIn[2+strBeg];
-
-            return byteNum+5;
-
-    }
-    //    else if (stringIn[strBeg+2] == 'C'){    // Characters of data to display
-    //            charMap(stringIn[strBeg+7], byteChar);
-    //            tempChar = stringIn[strBeg+7];
-    //            byteNum = twoCharConv (stringIn+strBeg+3);
-    //            shiftNum = stringIn[strBeg+5]-0x30;  // get the number of times to shift the data character
-    //            dispShift = stringIn[strBeg+6]-0x30;  // get the number of times to shift the existing display
-    //            shiftArr(gridArray,dispShift,5,uartdir);  // number of characters to shift the existing display
-    //            shiftIn(byteChar,gridArray+5-shiftNum,shiftNum,uartdir);  // New Data to display shifting in the desired number of bytes
-    //            stringOut[0] = stringIn[0+strBeg];
-    //            stringOut[1] = stringIn[1+strBeg];
-    //            stringOut[2] = stringIn[2+strBeg];
-    //            num2Char2Digit(byteNum-1,stringOut+3);
-    //            shiftArr(stringOut+5,1,byteNum,0);
-    //
-    //            return byteNum+4;
-    //
-    //        }
-    else if (stringIn[strBeg+2] == 'F'){    // Write data to Flash Memory
-
-    }
-    else if (stringIn[strBeg+2] == 'R'){    // Read request
-
-    }
-    else if (stringIn[strBeg+2] == 'W'){    // Response from read request
-
-    }
-    return 0;
-}
-
-void convNibble(char * stringIn,char * byteOut, int num2Conv){
-    // blindly converts two nibbles embedded in two bytes to a byte raw format
-    int k;
-    //    volatile char C1, C2;
-    //    volatile int tempVal=0;
-    for(k = 0;k<num2Conv;k++){
-        //        C1=stringIn[k*2];
-        //        C2 = stringIn[k*2+1];
-
-        //        tempVal = (stringIn[k*2]-0x30)*10 + (stringIn[k*2+1]-0x30);
-        byteOut[k] = (stringIn[k*2]-0x30)*10 + (stringIn[k*2+1]-0x30);
-    }
-}
-
-void num2Char2Digit(int valIn,char * stringIn){
-    //convert to a 2 digit char string.  Only 2 digits
-    int tempVal=0;
-    //    if (valIn<10){
-    //        stringIn[0]='0';
-    //        stringIn[1]=0x30+valIn;
-    //    }
-    tempVal = valIn/10;
-    stringIn[0]=tempVal+0x30;
-    valIn -= 10*tempVal;
-    stringIn[1] = valIn+0x30;
-
-}
-
-int twoCharConv (char * stringIn){
-    // simply and blindly creates a two digit int from two digit char
-    return (stringIn[0]-0x30)*10+(stringIn[1]-0x30);
-}
-
-int strComp (char * stringIn, char * strPattern, int stringLen, int patternLen){
-    // returns a value of where in the string the first occurance happens
-    int i,k,diffVal;
-    for (k=0;k<stringLen-patternLen;k++){
-        diffVal = 0;
-        for (i = 0;i<patternLen; i++){
-            if (stringIn[k+i] != strPattern[i])
-                diffVal++;
-        }
-        if (diffVal == 0)
-            return k;
-    }
-    return 0xFFFF;  // no pattern detected
-
 }
 
 
 void charMap(int valin, char * mapOut){
 
-    int k,n;
-    char tempval,holder;
-    for (k=0;k<5;k++){
-        holder=mapArray[valin-0x20][k];
-        tempval=0;
-        for (n=0;n<8;n++){
-            tempval<<=1;
-            if (holder&(1<<n)){
-
-                tempval+=1;
-
+    // needs
+    int k, n;
+    char tempval, holder;
+    for (k = 0; k < 5; k++) {
+        holder = mapArray[valin - 0x20][k];
+        tempval = 0;
+        for (n = 0; n < 8; n++) {
+            tempval <<= 1;
+            if (holder & (1 << n)) {
+                tempval += 1;
             }
-
-
-
         }
-        tempval>>=1;
-        mapOut[k]=tempval;
+        tempval >>= 1;
+        mapOut[k] = tempval;
     }
-
 }
+
 
 
 void test_sequence(int a_in, int k_in){  // Simply lights each LED by itself
@@ -358,28 +175,39 @@ void timer_ISR_init(void){
     _BIS_SR( GIE);                 // Enter LPM0 w/ interrupt
 }
 
-//// Port 1 interrupt service routine
-//#pragma vector=PORT1_VECTOR
-//__interrupt void Port_1(void)
-//{
-//    if (P1IFG & BIT0){
-//        P1IFG &= ~BIT0;                           // P1.0 IFG cleared
-//    }
-//    if (P1IFG & BIT3){
-//        P1IFG &= ~BIT3;                           // P1.4 IFG cleared
-//    }
-//}
-//
-//
-//// Port 1 interrupt service routine
-//#pragma vector=PORT2_VECTOR
-//__interrupt void Port_2(void)
-//{
-//    if (P2IFG & BIT6){
-//        P2IFG &= ~BIT6;                           // P1.0 IFG cleared
-//    }
-//
-//}
+// Port 1 interrupt service routine
+#pragma vector=PORT1_VECTOR
+__interrupt void Port_1(void)
+{
+    if (P1IFG & BIT0){
+        if (ModeSelect == 1){
+            ModeSelect = 1;
+        }
+        else{
+            ModeSelect ++;
+            if (ModeSelect > 4){
+                ModeSelect = 2;
+            }
+        }
+        P1IFG &= ~BIT0;                           // P1.0 IFG cleared
+
+    }
+    if (P1IFG & BIT3){
+        P1IFG &= ~BIT3;                           // P1.4 IFG cleared
+    }
+}
+
+
+// Port 1 interrupt service routine
+#pragma vector=PORT2_VECTOR
+__interrupt void Port_2(void)
+{
+    if (P2IFG & BIT6){
+        P2IFG &= ~BIT6;                           // P1.0 IFG cleared
+    }
+
+}
+
 
 // Timer A0 interrupt service routine
 #pragma vector=TIMER1_A0_VECTOR
@@ -390,6 +218,7 @@ __interrupt void Timer_A1 (void)
     P2OUT &=~ (1<<isrCount);
     P3OUT |= gridArray[isrCount]<<1;
     scrollCount++;
+    lockoutTimer ++;
     isrCount++;
     if (isrCount>4)
         isrCount=0;
